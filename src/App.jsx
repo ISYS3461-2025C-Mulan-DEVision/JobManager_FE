97cb44b3103c9155948406cbb2865556b51f8871
Nguyen Dinh Minh Chau <138189243+Annie-Chau@users.noreply.github.com>
2025-11-08 21:39:21 +0700

    React/Vite + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
