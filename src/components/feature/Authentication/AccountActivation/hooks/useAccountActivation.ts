d2ccc13032348ef6adef49df950272a01dbe12b9
Hanh Do <tranghanh.2702@gmail.com>
2025-12-21 11:02:36 +0700

    Fix account activation feature

--- src/components/feature/Authentication/AccountActivation/hooks/useAccountActivation.ts
+++ src/components/feature/Authentication/AccountActivation/hooks/useAccountActivation.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
-import { ActivationState } from "../types";
+import { ActivationState } from "../types.ts";
 
 export const useAccountActivation = () => {
     const [searchParams] = useSearchParams();
